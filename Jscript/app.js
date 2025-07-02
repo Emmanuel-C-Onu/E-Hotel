805acc16b8b59e90b36f7beefdc94742e34a6e35
Emmanuel Onu <sireconu@gmail.com>
2025-07-02 10:51:54 +0100

    implemented IntersectionObserver

--- Jscript/app.js
+++ Jscript/app.js
@@ -1,7 +1,8 @@
 const mnb = document.querySelector(".nav-menu-btn");
 const mnl = document.querySelector(".mobile-nav-links");
 const heda = document.querySelector(".header");
-const navContainer = document.querySelector(".nav-container");
+const nav = document.querySelector("nav");
+const cotana = document.querySelector(".cotana");
 
 new Splide("#splide", {
   type: "loop",
@@ -19,20 +20,17 @@ new Splide("#splide", {
 
 const addText = document.querySelector("#add-text");
 addText.innerHTML += `
-          <div class="hotelAnima text-center pb-10 text-white text-4xl font-[Times]
+          <div class="hotelAnima text-center mb-10 text-white text-4xl font-[Times]
                                   animate__animated animate__fadeInDownBig animate__delay-2 
                                   animate__slower">
            E-HOTEL AND LUXURY IS THE BEST FOR YOU !
           </div>
-          <p class="typeHere pb-10 text-center text-white text-2xl font-extralight"> </p>
+          <p class="typeHere my-10 text-center text-white text-2xl font-extralight"> </p>
           <div class=" place-items-center grid">
-            <button class="bookNow flex justify-center items-center 
-                         animate__animated animate__fadeInUpBig
-                         animate__delay-2 
-                                  animate__slower
-                            bg-gradient-to-r from-green-950 via-[green] to-green-950 
+            <button class="bookNow flex justify-center items-center  
+                         animate__animated animate__fadeInUpBig animate__delay-2 animate__slower
                          rounded py-2 px-9 text-white border border-[white]">
-              <span class="animate-pulse font-extralight">Book Now</span>
+              <span class=" font-extralight">Book Now</span>
             </button>
           </div>
 `;
@@ -56,6 +54,14 @@ document.addEventListener("DOMContentLoaded", function () {
   startTyping();
 });
 
+let scrollTimeout;
+window.addEventListener("scroll", () => {
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(() => {
+    cotana.classList.add("h-[155px]");
+  }, 60000);
+});
+
 const yesBtn = document.querySelector(".yesHelp");
 const noBtn = document.querySelector(".noHelp");
 
@@ -63,27 +69,19 @@ yesBtn.onclick = sayYes;
 noBtn.onclick = sayN0;
 
 function sayN0() {
-  const cotana = document.querySelector(".cotana");
   cotana.classList.remove("h-[155px]");
 }
 function sayYes() {
-  const cotana = document.querySelector(".cotana");
   alert("Coming Soon");
   cotana.classList.remove("h-[155px]");
 }
 
 function closeMobileNav() {
-  setTimeout(() => {
-    mnl.classList.remove("h-[450px]");
-    mnb.classList.remove("bg-[green]");
-  }, 1500);
+  mnl.classList.remove("h-[450px]");
 }
 mnb.addEventListener("click", () => {
   mnl.classList.toggle("h-[450px]");
-  mnb.classList.toggle("bg-[green]");
-  mnl.classList.add("*:bg-[#1F2D0E]");
 });
-heda.addEventListener("mouseleave", closeMobileNav);
 
 function b2t() {
   scrollTo({ top: 0, behavior: "smooth" });
@@ -126,9 +124,7 @@ form.addEventListener("submit", (e) => {
 
 closeModal.onclick = closeNewsletter;
 window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    showNewsletter();
-  }, 20000);
+  setTimeout(showNewsletter, 60000);
 });
 
 function closeNewsletter() {
@@ -169,3 +165,58 @@ function myElement() {
     });
   }
 }
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    nav.classList.add(
+      "fixed",
+      "top-0",
+      "left-0",
+      "w-full",
+      "bg-[#1F2D0E]",
+      "px-5"
+    );
+  } else {
+    nav.classList.remove(
+      "fixed",
+      "top-0",
+      "left-0",
+      "w-full",
+      "bg-[#1F2D0E]",
+      "px-5"
+    );
+  }
+});
+
+const cityContainer = document.querySelector(".city-container");
+const lagos = document.querySelector(".lagos");
+const abuja = document.querySelector(".abuja");
+const ph = document.querySelector(".ph");
+const owerri = document.querySelector(".owerri");
+
+let count = 0;
+
+function liveCount() {
+  count++;
+  if (count > 100) clearTimeout(intervalID);
+  let intervalID = setTimeout(liveCount, 50);
+
+  lagos.textContent = scale(count, 0, 100, 0, 3400).toFixed(0);
+  abuja.textContent = scale(count, 0, 100, 0, 2000).toFixed(0);
+  ph.textContent = scale(count, 0, 100, 0, 1400).toFixed(0);
+  owerri.textContent = scale(count, 0, 100, 0, 4000).toFixed(0);
+}
+
+function scale(x, a, b, c, d) {
+  return ((x - a) / (b - a)) * (d - c) + c;
+}
+
+function inView(entries) {
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      liveCount();
+    }
+  }
+}
+
+const monitor = new IntersectionObserver(inView, {threshold:0.75})
+monitor.observe(cityContainer);
